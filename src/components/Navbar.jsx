55f7c0ccc0a2c3ae9746c924421374b748464f66
shravani delta student <shravaniavula03@gmail.com>
2024-12-26 12:55:48 +0530

    add navbar and slidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 
-import{AppBar, Box, styled, Toolbar, Typography } from '@mui/material'
-import {Pets} from '@mui/icons-material';
+import{AppBar, Box, styled, Toolbar, Typography ,  InputBase, Avatar, Menu, MenuItem,  } from '@mui/material'
+import {  Pets} from '@mui/icons-material';
+import Badge from '@mui/material/Badge';
+import  Mail from '@mui/icons-material/Mail';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import img from './img-soc/client-1.jpg'
+import { useState } from 'react';
+
+
+
 
 
 const StyledToolbar =styled(Toolbar)({
@@ -10,23 +18,75 @@ const StyledToolbar =styled(Toolbar)({
 
 const Search =styled("div")(({theme}) =>({
 backgroundColor:"white",
+padding:"0 10px",
+borderRadius:theme.shape.borderRadius,
+width:"40%"
 }));
 
 const Icons =styled(Box)(({theme}) =>({
-  backgroundColor:"white",
+  display:"none",
+   gap:"20px",
+    alignItems:"center",
+     [theme.breakpoints.up("sm")]:{
+      display:"flex"
+     }
   }));
+
+  const UserBox =styled(Box)(({theme}) =>({
+    display:"flex",
+     gap:"10px",
+      alignItems:"center"
+    
+    }));
 const Navbar = () => {
+
+  const[open, setOpen] = useState(false)
   return (
     <Box sx={{flexGrow:1}}>
     <AppBar position='sticky' >
         <StyledToolbar>
             <Typography variant='h6' sx={{display:{xs:"none",sm:"block"}}}>navbar</Typography>
         <Pets sx={{display:{xs:"block",sm:"none"}}}/>
-       <Search>search</Search>
-       <Icons>icons</Icons>
+       <Search>< InputBase  placeholder='search...'/></Search>
+       <Icons>
+   <Badge badgeContent={4} color='error' >
+    <Mail />
+   </Badge>
+   <Badge badgeContent={2} color='error' >
+    < NotificationsActiveIcon/>
+   </Badge>
+   <Avatar 
+    sx={{width:30,height:30}}src={img}
+    onClick={e=>setOpen(true)} />
+       </Icons>
+
+<UserBox  onClick={e=>setOpen(true)}>
+<Avatar 
+    sx={{width:30,height:30}}src={img}
+    />
+    <Typography variant='span'>John</Typography>
+</UserBox>
         </StyledToolbar>
-        
-  
+        <Menu
+        id='demo-positioned-menu'
+        aria-labelledby='demo-positined-button'
+        open={open}
+        onClose={(e)=>setOpen(false)}
+        anchorOrigin={
+          {
+            vertical:'top',
+            horizontal:'right',
+          }
+        }
+        transformOrigin={{
+          vertical:'top',
+          horizontal:'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
     </AppBar>
     </Box>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,85 @@
-import { Box } from '@mui/material'
+import { Box} from '@mui/material'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import ArticleIcon from '@mui/icons-material/Article';
+import GroupIcon from '@mui/icons-material/Group';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="skyblue" flex={1} p={2} 
+    <Box
+     
+      flex={1} 
+      p={2} 
     sx={{display:{xs:"none" , sm:"block"}}}>
-      Sidebar
+      
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#home'>
+              <ListItemIcon>
+                <HomeIcon/>
+              </ListItemIcon>
+              <ListItemText primary="HomePage" />
+            </ListItemButton>
+          </ListItem>
+          
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#pages'>
+              <ListItemIcon>
+                <ArticleIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#groupes'>
+              <ListItemIcon>
+                <GroupIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#StorefrontIcon'>
+              <ListItemIcon>
+                <StorefrontIcon/>
+              </ListItemIcon>
+              <ListItemText primary="MarketPlace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#friends'>
+              <ListItemIcon>
+                <Diversity3Icon/>
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#Settings'>
+              <ListItemIcon>
+                <SettingsIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#profile'>
+              <ListItemIcon>
+                <AccountCircleIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          </List>
     </Box>
   )
 }
